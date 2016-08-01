301e873103b6a541f71ba19889d791d2f7802ce2
sultaniyad <developer.sultan@gmail.com>
2016-08-02 01:38:29 +0300

    Initial commit

---
+++ app/src/main/java/com/ayad/sultan/horrorstory/MainActivity.java
@@ -0,0 +1,26 @@
+package com.ayad.sultan.horrorstory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-2100187188382709~6532666677");
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+
+}
