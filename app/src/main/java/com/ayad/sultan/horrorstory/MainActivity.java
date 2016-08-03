2f76c2682f81e382e6c86d134d3fd1868e6406ba
sultaniyad <developer.sultan@gmail.com>
2016-08-03 16:55:20 +0300

    add new Design

--- app/src/main/java/com/ayad/sultan/horrorstory/MainActivity.java
+++ app/src/main/java/com/ayad/sultan/horrorstory/MainActivity.java
@@ -2,6 +2,8 @@ package com.ayad.sultan.horrorstory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -15,12 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+//addvertize
         MobileAds.initialize(getApplicationContext(), "ca-app-pub-2100187188382709~6532666677");
-
         AdView mAdView = (AdView) findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+
+        String arrayStory[] = getResources().getStringArray(R.array.stories);
+        ListView list =(ListView) findViewById(R.id.listView);
+        ArrayAdapter adpter = new ArrayAdapter(this,R.layout.listview_rows,R.id.storyTitle,arrayStory);
+
+        list.setAdapter(adpter);
     }
 
 }
